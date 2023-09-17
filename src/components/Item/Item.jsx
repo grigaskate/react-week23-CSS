4ffb7e820bec7d4a12e760feba5fc0615a14bc90
grigaskate <grigaskate@gmail.com>
2023-09-17 22:11:02 +0300

    correct Item.jsx

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,19 +1,16 @@
 import st from './style.module.scss'
 
-export default function Item(props) {
+
+export default function Item({item: {color, flag, rate, speed}}) {
+  const nameStyle = `${flag ? st.nameBig : st.name}`;
+
   return (
-    <div className={`${st.container} ${st[props.item.color]}`}>
-      <>
-        {props.item.flag ? (
-            <div className={st.nameBig}>Безлимитный {props.item.rate}</div>
-        ):(
-            <div className={st.name}>Безлимитный {props.item.rate}</div>
-        )}
-      </>
-      <div className={st.price}>{props.item.rate}</div>
-      <div className={st.rub} >руб/мес</div>
-      <div className={st.speed}>до {props.item.speed} Мбит/сек</div>
-      <div className={st.description}>Объем включенного трафика не ограничен</div>
+    <div className={`${st.container} ${st[color]}`}>
+    <div className={nameStyle}>Безлимитный {rate}</div>
+    <div className={st.price}>{rate}</div>
+    <div className={st.rub} >руб/мес</div>
+    <div className={st.speed}>до {speed} Мбит/сек</div>
+    <div className={st.description}>Объем включенного трафика не ограничен</div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import App from './components/App';
+import Test from './components/Test';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Test/>
   </React.StrictMode>
 );
 
