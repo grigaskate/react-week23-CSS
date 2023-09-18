700a36c2d3f9b625eaf759496f1a26c15bbaaf24
grigaskate <grigaskate@gmail.com>
2023-09-18 21:35:24 +0300

    delete Test.jsx

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -6,11 +6,11 @@ export default function Item({item: {color, flag, rate, speed}}) {
 
   return (
     <div className={`${st.container} ${st[color]}`}>
-    <div className={nameStyle}>Безлимитный {rate}</div>
-    <div className={st.price}>{rate}</div>
-    <div className={st.rub} >руб/мес</div>
-    <div className={st.speed}>до {speed} Мбит/сек</div>
-    <div className={st.description}>Объем включенного трафика не ограничен</div>
+      <div className={nameStyle}>Безлимитный {rate}</div>
+      <div className={st.price}>{rate}</div>
+      <div className={st.rub} >руб/мес</div>
+      <div className={st.speed}>до {speed} Мбит/сек</div>
+      <div className={st.description}>Объем включенного трафика не ограничен</div>
     </div>
   )
 }
